088466208d88c77a8af8c78cedfb78b942d4395a
JosephatEze <josephat.edu@gmail.com>
2023-01-25 00:43:54 +0100

    updated ec2 availability zone and ami

--- infra/instance/main.tf
+++ infra/instance/main.tf
@@ -61,7 +61,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "dot-public_subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-west-1a"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
   depends_on = [aws_internet_gateway.igw]
--- infra/instance/terraform.auto.tfvars
+++ infra/instance/terraform.auto.tfvars
@@ -1,3 +1,3 @@
-base_ami_id                 = "ami-0cabd..........d35d"
+base_ami_id                 = "ami-00874d747dde814fa"
 BUCKET_ID = "dot-bucket"
 public_key = "ec2_public_key"
