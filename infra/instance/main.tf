e4c546645aa6baaa51e2f7bdac193fc6faac3e5f
JosephatEze <josephat.edu@gmail.com>
2023-01-25 21:23:38 +0100

    added public route table

--- infra/instance/main.tf
+++ infra/instance/main.tf
@@ -71,6 +71,29 @@ resource "aws_subnet" "dot-public_subnet" {
   }
 }
 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_1_rt_a" {
+  subnet_id      = aws_subnet.dot-public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
