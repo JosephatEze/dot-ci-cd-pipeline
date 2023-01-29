1c21e0d422ce44e04445e43440fc0d59ca2da3ba
Eze Josephat Chinedu <josephat.edu@gmail.com>
2023-01-29 09:03:01 +0100

    updated main.tf file

--- infra/instance/main.tf
+++ infra/instance/main.tf
@@ -159,59 +159,6 @@ resource "aws_s3_bucket_policy" "public_read_access" {
 EOF
 }
 
-#Create an IAM Policy
-resource "aws_iam_policy" "s3-policy" {
-  name        = "S3-Bucket-Access-Policy"
-  description = "Provides permission to access S3"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:*",
-        ]
-        Effect   = "Allow"
-        Resource = [
-
-          "${aws_s3_bucket.dot-bucket.arn}",
-          "${aws_s3_bucket.dot-bucket.arn}/*"]
-      },
-    ]
-  })
-}
-
-#Create an IAM Role
-resource "aws_iam_role" "dot_server-role" {
-  name = "ec2_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = ["s3:*"]
-        Effect = "Allow"
-        Sid    = "RoleForEC2"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "s3-policy-attach" {
-  name       = "policy-attachment"
-  roles      = [aws_iam_role.dot_server-role.name]
-  policy_arn = aws_iam_policy.s3-policy.arn
-}
-
-#Create an instance profile using role
-resource "aws_iam_instance_profile" "dot_server_profile" {
-  name = "server_profile"
-  role = aws_iam_role.dot_server-role.name
-}
-
 resource "aws_instance" "dot_server" {
   ami                    = var.base_ami_id
   instance_type          = "t2.micro"
